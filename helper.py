bc119f69f0e05de4374cec4fcfdf7f1e4ae90da3
Ryan Henning <ryan@rhobota.com>
2016-12-13 17:09:30 -0600

    Add galvanize theme resources

--- helper.py
+++ helper.py
@@ -2,6 +2,6 @@ import matplotlib.image as image
 
 
 def watermark_fig(fig, x=50, y=50):
-    datafile = '/Users/ryan/Desktop/store/galvanize_logo/ATX Galvanize Data Science (small).png'
+    datafile = 'galvanize_theme_resources/ATX Galvanize Data Science (small).png'
     im = image.imread(datafile)
     fig.figimage(im, x, y, zorder=10, alpha=1.0, origin='upper')
